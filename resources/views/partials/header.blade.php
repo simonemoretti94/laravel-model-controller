5c29be1be7ec5ff8733bf130fd0711a010677ea6
simonemoretti94 <simone.moretti.944@gmail.com>
2024-04-18 15:49:32 +0200

    4th added nav to header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,9 @@
-<div>
-header
+<div class="container-fluid">
+    <div class="row">
+        <nav>
+        <a href="{{route('index')}}">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contacts</a>
+        </nav>
+    </div>
 </div>
