e9409e3f9a59da60908d6951e6be5d9ea992dc28
simonemoretti94 <simone.moretti.944@gmail.com>
2024-04-18 17:02:02 +0200

    4.1th styling cards

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,8 +11,8 @@ index
 <div class="container bg-dark">
     <div class="row gy-1">
         @foreach ($movies_data as $movie)
-        <div class="col-lg-4 col-md-6 col-sm-12 p-1">
-            <div class="card d-flex flex-column">
+        <div class="col-lg-4 col-md-6 col-sm-6 col-12 p-1">
+            <div class="card d-flex flex-column" style="min-height: 160px;">
             <div class="card-header">
             <p>{{$movie['title']}}</p>    
             </div>
