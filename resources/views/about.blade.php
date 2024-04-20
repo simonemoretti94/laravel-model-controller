e7c98b67483fd5bd658373f504089036143432cf
simonemoretti94 <simone.moretti.944@gmail.com>
2024-04-20 22:04:15 +0200

    8th added content into about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,6 +5,17 @@
 @section('content')
 
 <div>
+<div class="container" >
+    <h2 class="text-center text-white" >Whoops!</h2>
+    <p class="text-center text-primary mt-5">
+    I apologize for any inconvenience you may have experienced while searching for content on our website. Unfortunately, it seems that the information you were looking for is currently unavailable. We are actively working to rectify this issue and appreciate your patience.
+
+    If there’s anything else we can assist you with, please feel free to reach out. Thank you for your understanding.
+
+    The staff | back here: <a href="{{route('index')}}" class="text-white text-center">Home</a>
+    </p>
+
+</div>
 </div>
 
 @endsection
--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -9,7 +9,7 @@
         <p>
         <img class="border border-2 max-h-32" src="https://picsum.photos/200/300" alt="lorem">
         </p>
-        <p class="mt-5 text-primary"><b><i>Contacts page | </i></b> if you are interested in my portofolio works, please contact me at <u class="text-primary">'simone.moretti.944@gmail.com'</u></p>
+        <p class="mt-5 text-primary"><b><i>Contacts page | </i></b> if you are interested in my portofolio works, please contact me at <a href="{{route('index')}}" class="text-primary">simone.moretti.944@gmail.com</a></p>
 </div>
 
 @endsection
