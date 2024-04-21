cbd8e2842199e285183655d3ebc06a0d830e6263
simonemoretti94 <simone.moretti.944@gmail.com>
2024-04-21 22:11:07 +0200

    9.1th changing header nav class

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,8 +2,8 @@
 <nav
     class="nav justify-content-evenly">
     <a class="nav-link {{ request()->is('/') ? 'bg-active' : ''}}" href="{{route('index')}}" aria-current="page">Home</a>
-    <a class="nav-link {{ request()->is('about') ? 'bg-active' : ''}}" href="{{route('about')}}">About</a>
-    <a class="nav-link {{ request()->is('contacts') ? 'bg-active' : ''}}" href="{{route('contacts')}}">Contacts</a>
+    <a class="nav-link {{ request()->is('about') ? 'bg-active' : ''}} {{!Route::currentRouteName('/' || 'about' || 'contacts') ? 'd-none' : ''}}" href="{{route('about')}}">About</a>
+    <a class="nav-link {{ request()->is('contacts') ? 'bg-active' : ''}} {{!Route::currentRouteName('/' || 'about' || 'contacts') ? 'd-none' : ''}}" href="{{route('contacts')}}">Contacts</a>
 </nav>
 </div>
 
