a06fcba5c04b57a389cb19dc693c290d8899b2e6
simonemoretti94 <simone.moretti.944@gmail.com>
2024-04-22 18:40:11 +0200

    10.1th restyling index's ul anchor tags

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -22,15 +22,18 @@ index
         </div>
         @endforeach
 
-        <ul class="bg-light w-50 mx-auto my-2 rounded rounded-2 list-unstyled">
+        <div class="container">
+        <ul class="w-50 mx-auto my-2 ms-0">
         @foreach ($movies_data as $movie)
-            <li class=" text-left" >
+            <li style="text-align: left !important;">
                 <a href="/movie/{{$movie['original_title']}}" class="text-primary">
-                    <strong>{{$movie['id']}} | </strong> {{$movie['original_title']}}
+                    <strong class="text-white">{{$movie['id']}} | </strong> {{$movie['original_title']}}
                 </a>
             </li>    
         @endforeach
         </ul>
+        </div>
+
     </div>
     <h2 style="color: white;">array length: {{count($movies_data)}}</h2>
 </div>
